bbbf61bc9998f448d58c73f94377bc5b3173c631
caiocmb7 <caiobarros@alu.ufc.br>
2022-08-25 21:46:41 -0300

    new version

--- ML&AI/Vehicle License Plate Detection/class.py
+++ ML&AI/Vehicle License Plate Detection/class.py
@@ -13,16 +13,17 @@ import os
 class Car:
 
   # init method or constructor
-  def __init__(self, path):
-      self.path = path
+  def __init__(self):
+      pass
 
-  def __filters(img):
+  def __filters(self, img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
     edged = cv2.Canny(bfilter, 30, 200) #Edge detection
-    return gray, bfilter, edged
+    return gray, edged
   
-  def __search_plate_and_crop(edged):
+  def __search_plate_and_crop(self, img):
+    gray, edged = self.__filters(img)
     keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(keypoints)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
@@ -41,30 +42,31 @@ class Car:
     (x1, y1) = (np.min(x), np.min(y))
     (x2, y2) = (np.max(x), np.max(y))
     cropped_image = gray[x1:x2+1, y1:y2+1]
-    return cropped_image
+    return new_image, approx, cropped_image
 
-    # Sample Method
-    def plateDetection(self):
-      df_lista = []
+  # Sample Method
+  def plateDetection(self, path):
+    self.path = path
+    df_lista = []
 
-      for dir, subarch, archives in os.walk(path):
-        for path_imagem in archives:
-          img = cv2.imread(path + "/" + str(path_imagem))
-          __filters(img)
-          __search_plate_and_crop(edged)
+    for dir, subarch, archives in os.walk(path):
+      for path_imagem in archives:
+        img = cv2.imread(path + "/" + str(path_imagem))
+        self.__filters(img)
+        self.__search_plate_and_crop(img, self.edged, self.gray)
 
-          reader = easyocr.Reader(['en'])
-          result = reader.readtext(cropped_image)
+        reader = easyocr.Reader(['en'])
+        result = reader.readtext(self.cropped_image)
 
-          text = result[0][-2]
-          df_lista.append((path_imagem, text))
-          font = cv2.FONT_HERSHEY_SIMPLEX
-          res = cv2.putText(img, text=text, org=(approx[1][0][0], approx[2][0][1]+30), fontFace=font, fontScale=0.6, color=(0,255,0), thickness=3, lineType=cv2.LINE_AA)
-          res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
-          plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
-          plt.savefig(f"/content/detection/{path_imagem}")
+        text = result[0][-2]
+        df_lista.append((path_imagem, text))
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        res = cv2.putText(img, text=text, org=(self.approx[1][0][0], self.approx[2][0][1]+30), fontFace=font, fontScale=0.6, color=(0,255,0), thickness=3, lineType=cv2.LINE_AA)
+        res = cv2.rectangle(img, tuple(self.approx[0][0]), tuple(self.approx[2][0]), (0,255,0),3)
+        plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+        plt.savefig(f"/content/detection/{path_imagem}")
 
-      df_aux = pd.DataFrame(df_lista)
-      df_aux.rename(columns={0: "Image", 1: "Plate"}, inplace = True)
-      df = df_aux.to_csv("/content/detection/results.csv", index = False)
-      return df
+    df_aux = pd.DataFrame(df_lista)
+    df_aux.rename(columns={0: "Image", 1: "Plate"}, inplace = True)
+    df = df_aux.to_csv("/content/detection/results.csv", index = False)
+    return df
