cb3b238dfde726d0f50f3a0726dee15bdcd9e2a6
caiocmb7 <caiobarros@alu.ufc.br>
2022-09-22 16:44:02 -0300

    class formatted

--- ML&AI/Vehicle License Plate Detection/class.py
+++ ML&AI/Vehicle License Plate Detection/class.py
@@ -9,141 +9,170 @@ import imutils
 import pandas as pd
 import os
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 # A Sample class with init method
 class Car:
 
-  # init method or constructor
-  def __init__(self):
-      pass
+    # init method or constructor
+    def __init__(self):
+        pass
 
-  def __filters(self, img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
-    edged = cv2.Canny(bfilter, 30, 200) #Edge detection
-    return gray, bfilter, edged
-  
-  def __search_plate_and_crop(self, img, edged, gray):
-    keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(keypoints)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
-    
-    location = 0
-    for contour in contours:
-        approx = cv2.approxPolyDP(contour, 10, True)
-        if len(approx) == 4:
-            location = approx
-            break
-    mask = np.zeros(gray.shape, np.uint8)
-    new_image = cv2.drawContours(mask, [location], 0,255, -1)
-    new_image = cv2.bitwise_and(img, img, mask=mask)
+    def __filters(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        bfilter = cv2.bilateralFilter(gray, 11, 17, 17)  # Noise reduction
+        edged = cv2.Canny(bfilter, 30, 200)  # Edge detection
+        return gray, bfilter, edged
 
-    (x,y) = np.where(mask==255)
-    (x1, y1) = (np.min(x), np.min(y))
-    (x2, y2) = (np.max(x), np.max(y))
-    cropped_image = gray[x1:x2+1, y1:y2+1]
-    return new_image, approx, cropped_image
+    def __search_plate_and_crop(self, img, edged, gray):
+        keypoints = cv2.findContours(
+            edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )
+        contours = imutils.grab_contours(keypoints)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
-  def __create_new_folder(self, path, folder_name): 
-    try: 
-      current_path = os.getcwd() 
-      os.chdir('.')
-      path = os.getcwd()
-      full_path = os.path.join(path, folder_name) 
-      os.makedirs(full_path) 
-      os.chdir(current_path)
-      return full_path
-    except OSError as error: 
-      print(f"Already have a folder called: {folder_name} in this directory")
-      full_path = os.path.join(path, folder_name)
-      return full_path 
+        location = 0
+        for contour in contours:
+            approx = cv2.approxPolyDP(contour, 10, True)
+            if len(approx) == 4:
+                location = approx
+                break
+        mask = np.zeros(gray.shape, np.uint8)
+        new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+        new_image = cv2.bitwise_and(img, img, mask=mask)
 
-  def __plot_images(self, img1, img2, title1="", title2=""):
-    fig = plt.figure(figsize=[15,15])
-    ax1 = fig.add_subplot(121)
-    ax1.imshow(img1, cmap="gray")
-    ax1.set(xticks=[], yticks=[], title=title1)
+        (x, y) = np.where(mask == 255)
+        (x1, y1) = (np.min(x), np.min(y))
+        (x2, y2) = (np.max(x), np.max(y))
+        cropped_image = gray[x1 : x2 + 1, y1 : y2 + 1]
+        return new_image, approx, cropped_image
 
-    ax2 = fig.add_subplot(122)
-    ax2.imshow(img2, cmap="gray")
-    ax2.set(xticks=[], yticks=[], title=title2)
-    
-    return fig
-
-  # Sample Method
-  def plateDetection(self, path, folder_name, show_steps = False):
-    self.path = path
-    self.folder_name = folder_name
-    df_lista = []
-
-    full_path = self.__create_new_folder(path, folder_name)
-
-    for dir, subarch, archives in os.walk(path):
-      for path_imagem in archives:
+    def __create_new_folder(self, path, folder_name):
         try:
-          img = cv2.imread(path + "/" + str(path_imagem))
-          gray, bfilter, edged  = self.__filters(img)
-          new_image, approx, cropped_image = self.__search_plate_and_crop(img, edged, gray)
+            current_path = os.getcwd()
+            os.chdir(".")
+            path = os.getcwd()
+            full_path = os.path.join(path, folder_name)
+            os.makedirs(full_path)
+            os.chdir(current_path)
+            return full_path
+        except OSError as error:
+            print(f"Already have a folder called: {folder_name} in this directory")
+            full_path = os.path.join(path, folder_name)
+            return full_path
 
-          reader = easyocr.Reader(['en'])
-          result = reader.readtext(cropped_image)
+    def __plot_images(self, img1, img2, title1="", title2=""):
+        fig = plt.figure(figsize=[15, 15])
+        ax1 = fig.add_subplot(121)
+        ax1.imshow(img1, cmap="gray")
+        ax1.set(xticks=[], yticks=[], title=title1)
 
-          text = result[0][-2]
-          df_lista.append((path_imagem, text))
-          font = cv2.FONT_HERSHEY_SIMPLEX
-          res = cv2.putText(img, text=text, org=(approx[1][0][0], approx[2][0][1]+30), fontFace=font, fontScale=0.7, color=(0,255,0), thickness=3, lineType=cv2.LINE_AA)
-          res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
-          plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
-          plt.savefig(full_path + "/" + path_imagem)
-        except IndexError as IE:
-          print(f"\n\nOcorreu um erro de Index na imagem: {path_imagem}, porém continuando para a proxima imagem")
-          continue
-        except Exception as error:
-          print(f"\n\nOcorreu um erro na imagem: {path_imagem}, porém continuando para a proxima imagem")
-          continue
+        ax2 = fig.add_subplot(122)
+        ax2.imshow(img2, cmap="gray")
+        ax2.set(xticks=[], yticks=[], title=title2)
 
-    df_aux = pd.DataFrame(df_lista)
-    df_aux.rename(columns={0: "Image", 1: "Plate"}, inplace = True)
-    df = df_aux.to_csv(full_path + "/" + "results.csv", index = False)
+        return fig
 
-    if show_steps == True:
-      self.__plot_images(img, gray, title1="original", title2="gray")
-      self.__plot_images(gray, bfilter, title1="gray", title2="bfilter")
-      self.__plot_images(bfilter, edged, title1="bfilter", title2="edged")
-      self.__plot_images(img, cropped_image, title1="original", title2="cropped_image")
+    # Sample Method
+    def plateDetection(self, path, folder_name, show_steps=False):
+        self.path = path
+        self.folder_name = folder_name
+        df_lista = []
 
-    return df
-    
-  def YOLOplateDetection(self, path, folder_name):
-    self.path = path
-    self.folder_name = folder_name
-    df_lista_yolo = []
+        full_path = self.__create_new_folder(path, folder_name)
 
-    full_path = self.__create_new_folder(path, folder_name)
+        for dir, subarch, archives in os.walk(path):
+            for path_imagem in archives:
+                try:
+                    img = cv2.imread(path + "/" + str(path_imagem))
+                    gray, bfilter, edged = self.__filters(img)
+                    new_image, approx, cropped_image = self.__search_plate_and_crop(
+                        img, edged, gray
+                    )
 
-    for dir, subarch, archives in os.walk(path):
-      for path_imagem in archives:
-        try:
-          img = cv2.imread(path + "/" + str(path_imagem))
-          img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-          reader = easyocr.Reader(['en'])
-          result = reader.readtext(img)
-          text = result[0][-2]
-          df_lista_yolo.append((path_imagem, text))
+                    reader = easyocr.Reader(["en"])
+                    result = reader.readtext(cropped_image)
 
-        except IndexError as IE:
-          print(f"\n\nOcorreu um erro de Index na imagem: {path_imagem}, porém continuando para a proxima imagem")
-          continue
-        except Exception as error:
-          print(f"\n\nOcorreu um erro na imagem: {path_imagem}, porém continuando para a proxima imagem")
-          continue
+                    text = result[0][-2]
+                    df_lista.append((path_imagem, text))
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    res = cv2.putText(
+                        img,
+                        text=text,
+                        org=(approx[1][0][0], approx[2][0][1] + 30),
+                        fontFace=font,
+                        fontScale=0.7,
+                        color=(0, 255, 0),
+                        thickness=3,
+                        lineType=cv2.LINE_AA,
+                    )
+                    res = cv2.rectangle(
+                        img, tuple(approx[0][0]), tuple(approx[2][0]), (0, 255, 0), 3
+                    )
+                    plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+                    plt.savefig(full_path + "/" + path_imagem)
+                except IndexError as IE:
+                    print(
+                        f"\n\nOcorreu um erro de Index na imagem: {path_imagem}, porém continuando para a proxima imagem"
+                    )
+                    continue
+                except Exception as error:
+                    print(
+                        f"\n\nOcorreu um erro na imagem: {path_imagem}, porém continuando para a proxima imagem"
+                    )
+                    continue
 
-    df_aux = pd.DataFrame(df_lista_yolo)
-    df_aux.rename(columns={0: "Image", 1: "Plate"}, inplace = True)
-    df = df_aux.sort_values("Image").to_csv(full_path + "/" + "yolo_results.csv", index = False)
+        df_aux = pd.DataFrame(df_lista)
+        df_aux.rename(columns={0: "Image", 1: "Plate"}, inplace=True)
+        df = df_aux.to_csv(full_path + "/" + "results.csv", index=False)
 
-    return df
+        if show_steps == True:
+            self.__plot_images(img, gray, title1="original", title2="gray")
+            self.__plot_images(gray, bfilter, title1="gray", title2="bfilter")
+            self.__plot_images(bfilter, edged, title1="bfilter", title2="edged")
+            self.__plot_images(
+                img, cropped_image, title1="original", title2="cropped_image"
+            )
+
+        return df
+
+    def YOLOplateDetection(self, path, folder_name):
+        self.path = path
+        self.folder_name = folder_name
+        df_lista_yolo = []
+
+        full_path = self.__create_new_folder(path, folder_name)
+
+        for dir, subarch, archives in os.walk(path):
+            for path_imagem in archives:
+                try:
+                    img = cv2.imread(path + "/" + str(path_imagem))
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    reader = easyocr.Reader(["en"])
+                    result = reader.readtext(img)
+                    text = result[0][-2]
+                    df_lista_yolo.append((path_imagem, text))
+
+                except IndexError as IE:
+                    print(
+                        f"\n\nOcorreu um erro de Index na imagem: {path_imagem}, porém continuando para a proxima imagem"
+                    )
+                    continue
+                except Exception as error:
+                    print(
+                        f"\n\nOcorreu um erro na imagem: {path_imagem}, porém continuando para a proxima imagem"
+                    )
+                    continue
+
+        df_aux = pd.DataFrame(df_lista_yolo)
+        df_aux.rename(columns={0: "Image", 1: "Plate"}, inplace=True)
+        df = df_aux.sort_values("Image").to_csv(
+            full_path + "/" + "yolo_results.csv", index=False
+        )
+
+        return df
+
 
 # To run the class above, do:
 
